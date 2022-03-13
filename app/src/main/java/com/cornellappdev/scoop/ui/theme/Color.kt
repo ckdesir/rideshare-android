931103aaa270958fa7732d82f98e0ef31eec4e1f
ckdesir <christopher.desir01@gmail.com>
2022-03-13 03:28:42 -0400

    Add LightGray and PlaceholderGray to Colors

--- app/src/main/java/com/cornellappdev/scoop/ui/theme/Color.kt
+++ app/src/main/java/com/cornellappdev/scoop/ui/theme/Color.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.graphics.Color
 
 val Gray = Color(0xFFE8E8E8)
 val DarkGray = Color(0xFFAFACAC)
+val LightGray = Color(0xFFECECEC)
+val PlaceholderGray = Color(0xFFC4C4C4)
 
 val ColorPalette = lightColors(
     background = Color.White,
