f3e9b08b3846be2f2b6d0717e41b2938b5e14260
Justin Jiang <justin@Justins-MacBook-Pro-2.local>
2022-03-17 15:37:00 -0400

    random stuff

---
+++ app/src/main/java/com/cornellappdev/scoop/onboarding/OnboardingView1.kt
@@ -0,0 +1,12 @@
+package com.cornellappdev.scoop.onboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import com.cornellappdev.scoop.ScoopApp
+
+@Composable
+fun OnboardingScreen1(){
+
+}
