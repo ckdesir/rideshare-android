f69006d558b9a7180a078bab55122194d950f2ff
Justin Jiang <justin@dhcp-vl2042-23018.redrover.cornell.edu>
2022-03-23 17:59:55 -0400

    add profile image

--- app/src/main/java/com/cornellappdev/scoop/onboarding/OnboardingView1.kt
+++ app/src/main/java/com/cornellappdev/scoop/onboarding/OnboardingView1.kt
@@ -1,16 +1,26 @@
 package com.cornellappdev.scoop.onboarding
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,43 +57,111 @@ fun OnboardingView1(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ProfileImage(){
-    Box(
-    ){
-        Card(
-            modifier = Modifier.size(120.dp),
-            shape = CircleShape,
-            elevation = 2.dp,
-            backgroundColor = colorResource(id = R.color.lightgray)
-        ) {
-//        Image(
-//            painterResource(R.drawable.ic_home_icon),
-//            contentDescription = "",
-//            contentScale = ContentScale.Crop,
-//            modifier = Modifier.fillMaxSize()
-//        )
-        }
 
-        Card(
-            modifier = Modifier
-                .size(25.dp)
-                .align(Alignment.BottomEnd),
-            shape = CircleShape,
-            elevation = 2.dp,
-            backgroundColor = colorResource(id = R.color.lightgray),
-            border = BorderStroke(width = 2.dp, color = Color.White)
-        ) {
-            Image(
-                painterResource(R.drawable.ic_baseline_add_24),
-                contentDescription = "",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+    val bitmap =  remember {
+        mutableStateOf<Bitmap?>(null)
+    }
 
+    val launcher = rememberLauncherForActivityResult(contract =
+    ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
+    
+    if(imageUri == null) {
+        Box(
+        ) {
+            Card(
+                modifier = Modifier.size(120.dp),
+                shape = CircleShape,
+                elevation = 2.dp,
+                backgroundColor = colorResource(id = R.color.lightgray)
+            ) {}
+
+            Card(
+                modifier = Modifier
+                    .size(25.dp)
+                    .align(Alignment.BottomEnd),
+                shape = CircleShape,
+                elevation = 2.dp,
+                backgroundColor = colorResource(id = R.color.lightgray),
+                border = BorderStroke(width = 2.dp, color = Color.White),
+                onClick = {
+                    launcher.launch("image/*")
+                }
+            ) {
+                Image(
+                    painterResource(R.drawable.ic_baseline_add_24),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
+    else{
+
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images
+                    .Media.getBitmap(context.contentResolver, it)
+
+            } else {
+                val source = ImageDecoder
+                    .createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+
+            bitmap.value?.let { btm ->
+                Box(
+                ) {
+                    Card(
+                        modifier = Modifier.size(120.dp),
+                        shape = CircleShape,
+                        elevation = 2.dp,
+                        backgroundColor = colorResource(id = R.color.lightgray)
+                    ) {
+                        Image(
+                            bitmap = btm.asImageBitmap(),
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+
+                    Card(
+                        modifier = Modifier
+                            .size(25.dp)
+                            .align(Alignment.BottomEnd),
+                        shape = CircleShape,
+                        elevation = 2.dp,
+                        backgroundColor = colorResource(id = R.color.lightgray),
+                        border = BorderStroke(width = 2.dp, color = Color.White),
+                        onClick = {
+                            launcher.launch("image/*")
+                        }
+                    ) {
+                        Image(
+                            painterResource(R.drawable.ic_baseline_add_24),
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+
+
+                }
+            }
+        }
 
     }
+
 }
 
 @Composable
