db5342895a7ae45de86b2ba2e5e6120677857e35
Justin Jiang <justin@dhcp-vl2042-1111.redrover.cornell.edu>
2022-03-31 18:18:39 -0400

    move stuff

---
+++ app/src/main/java/com/cornellappdev/scoop/ui/onboarding/OnboardingView2.kt
@@ -0,0 +1,129 @@
+package com.cornellappdev.scoop.onboarding
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cornellappdev.scoop.components.DenseTextField
+import com.cornellappdev.scoop.components.RightArrow
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun OnboardingView2(pagerState: PagerState) {
+
+    val (nameText, setNameText) = rememberSaveable { mutableStateOf("") }
+    val (pronounsText, setPronounsText) = rememberSaveable { mutableStateOf("") }
+    val (hometownText, setHometownText) = rememberSaveable { mutableStateOf("") }
+    val (yearText, setYearText) = rememberSaveable { mutableStateOf("") }
+
+    Scaffold(
+
+        modifier = Modifier
+            .padding(
+                start = 40.dp,
+                end = 40.dp,
+            )
+            .fillMaxWidth()
+    ) {
+
+        Column(
+            Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(25.dp)
+        ) {
+            OnboardingHeader(pagerState = pagerState, "About You")
+            Spacer(modifier = Modifier.weight(1F))
+
+            Box(){
+                Column(Modifier.fillMaxWidth(),) {
+                    Text(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Default,
+                        text = "Name",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(
+                            bottom = 10.dp
+                        )
+                    )
+                    DenseTextField(value = nameText, setValue = setNameText, placeholderText = "Enter Name")
+                }
+
+            }
+
+            Box(Modifier.fillMaxWidth()){
+                Column() {
+                    Text(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Default,
+                        text = "Pronouns",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(
+                            bottom = 10.dp
+                        )
+                    )
+                    DenseTextField(value = pronounsText, setValue = setPronounsText, placeholderText = "Enter Pronouns")
+                }
+
+            }
+
+            Box(Modifier.fillMaxWidth()){
+
+                Column() {
+                    Text(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Default,
+                        text = "Name",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(
+                            bottom = 10.dp
+                        )
+                    )
+                    DenseTextField(value = hometownText, setValue = setHometownText, placeholderText = "Enter Hometown")
+                }
+
+            }
+
+            Box(Modifier.fillMaxWidth()){
+                Column() {
+                    Text(
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Default,
+                        text = "Class Year",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(
+                            bottom = 10.dp
+                        )
+                    )
+                    DenseTextField(value = yearText, setValue = setYearText, placeholderText = "Enter Class Year")
+                }
+
+            }
+
+            Box(
+
+                modifier = Modifier.align(Alignment.End)
+            ){
+                RightArrow(pagerState)
+            }
+
+            Spacer(modifier = Modifier.weight(1.5F))
+
+        }
+    }
+}
+
+
+//@Preview
+//@Composable
+//fun PreviewOnboard(){
+//    OnboardingView2(pagerState)
+//}
---
+++ app/src/main/java/com/cornellappdev/scoop/ui/onboarding/OnboardingView4.kt
@@ -0,0 +1,160 @@
+package com.cornellappdev.scoop.onboarding
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cornellappdev.scoop.components.RightArrow
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.PagerState
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun OnboardingView4(
+    pagerState : PagerState
+) {
+    
+    Scaffold() {
+        
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            OnboardingHeader(pagerState = pagerState, title = "During Roadtrips ...")
+
+            Spacer(modifier = Modifier.weight(1F))
+
+            var sliderPosition1 = remember { mutableStateOf(0F)}
+            var sliderPosition2 = remember { mutableStateOf(0F)}
+            Text(
+                fontFamily = FontFamily.Default,
+                text = "How talkative are you?",
+                fontSize = 20.sp,
+            )
+
+            OnboardingSlider(values = listOf("Quiet", " ", " ", "Talkative"), sliderPosition = sliderPosition1)
+
+            Text(
+                fontFamily = FontFamily.Default,
+                text = "Do you like music?",
+                fontSize = 20.sp,
+            )
+
+            OnboardingSlider(values = listOf("No Music", " ", " ", "Music"), sliderPosition = sliderPosition2)
+
+            Box(
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(
+                        end = 30.dp,
+                        top = 20.dp,
+                    )
+
+            ){
+                RightArrow(pagerState)
+            }
+
+            Spacer(modifier = Modifier.weight(1.25F))
+
+        }
+
+    }
+    
+
+}
+
+@Composable
+fun VerticalLines(values: List<String>) {
+
+    val drawPadding = with(LocalDensity.current) { 10.dp.toPx() }
+    val textSize = with(LocalDensity.current) {15.dp.toPx() }
+    val lineHeightDp = 10.dp
+    val lineHeightPx = with(LocalDensity.current) { lineHeightDp.toPx() }
+    val canvasHeight = 50.dp
+    val textPaint = android.graphics.Paint().apply {
+        color = if (isSystemInDarkTheme()) 0xffffffff.toInt() else 0xffff47586B.toInt()
+        textAlign = android.graphics.Paint.Align.CENTER
+        this.textSize = textSize
+    }
+
+    Box(contentAlignment = Alignment.Center) {
+        Canvas(
+            modifier = Modifier
+                .height(canvasHeight)
+                .fillMaxWidth()
+                .padding(
+                    top = canvasHeight
+                        .div(2)
+                        .minus(lineHeightDp.div(2))
+                )
+        ) {
+            val yStart = 0f
+            val distance = (size.width.minus(2 * drawPadding)).div(values.size.minus(1))
+            values.forEachIndexed { index, step ->
+                drawLine(
+                    color = Color.Black,
+                    start = Offset(x = drawPadding + index.times(distance), y = yStart),
+                    end = Offset(x = drawPadding + index.times(distance), y = lineHeightPx),
+                    strokeWidth = 8F,
+                )
+                if (index.rem(3) == 0) {
+                    this.drawContext.canvas.nativeCanvas.drawText(
+                        step,
+                        drawPadding + index.times(distance),
+                        size.height,
+                        textPaint
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun OnboardingSlider(values: List<String>, sliderPosition: MutableState<Float>) {
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.padding(
+            start = 20.dp,
+            end = 20.dp,
+            top = 10.dp,
+            bottom = 10.dp,
+        )
+    ) {
+        VerticalLines(values)
+        Slider(modifier = Modifier.fillMaxWidth(),
+            value = sliderPosition.value,
+            onValueChange = {
+                sliderPosition.value = it},
+            colors = customSliderColors()
+        )
+    }
+}
+
+@Composable
+private fun customSliderColors(): SliderColors = SliderDefaults.colors(
+    activeTickColor = Color.Transparent,
+    inactiveTickColor = Color.Transparent,
+    inactiveTrackColor = Color.Black,
+    activeTrackColor = Color.Black,
+    thumbColor = Color.Black
+)
+//
+//
+//@Preview
+//@Composable
+//fun PreviewOnboard(){
+//    OnboardingView4()
+//}
