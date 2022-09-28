729a6665b41273a0a7637483b337cc36d1883de5
Maxwell Pang <maxwell.amao@gmail.com>
2022-09-28 18:05:24 -0400

    Create User, Ride, and Path models   Create empty files for ride repository and ride request body

--- app/src/main/java/com/cornellappdev/scoop/data/models/User.kt
+++ app/src/main/java/com/cornellappdev/scoop/data/models/User.kt
@@ -3,10 +3,10 @@ package com.cornellappdev.scoop.data.models
 data class User(
     var id: Int? = null,
     var netid: String? = null,
-    var first_name: String? = null,
-    var last_name: String? = null,
-    var phone_number: String? = null,
+    var firstName: String? = null,
+    var lastName: String? = null,
+    var phoneNumber: String? = null,
     var grade: String? = null,
-    var profile_pic_url: String? = null,
+    var profilePicUrl: String? = null,
     var pronouns: String? = null
 )
