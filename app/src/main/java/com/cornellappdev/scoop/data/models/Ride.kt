d804ae56a38a1e74a23081abee765f44bc506f18
Maxwell Pang <maxwell.amao@gmail.com>
2022-09-28 18:31:48 -0400

    Put Moshi JSON markers for the properties of classes

--- app/src/main/java/com/cornellappdev/scoop/data/models/Ride.kt
+++ app/src/main/java/com/cornellappdev/scoop/data/models/Ride.kt
@@ -1,13 +1,15 @@
 package com.cornellappdev.scoop.data.models
 
+import com.squareup.moshi.Json
+
 data class Ride(
     var id: Int? = null,
     var creator: User? = null,
-    var maxTravelers: Int? = null,
-    var minTravelers: Int? = null,
-    var departure_datetime: String? = null,
+    @Json(name = "max_travelers") var maxTravelers: Int? = null,
+    @Json(name = "min_travelers") var minTravelers: Int? = null,
+    @Json(name = "departure_datetime") var departureDatetime: String? = null,
     var description: String? = null,
-    var isFlexible: Boolean? = null,
+    @Json(name = "is_flexible") var isFlexible: Boolean? = null,
     var type: String? = null,
     var departureLocation: String? = null,
     var arrivalLocation: String? = null,
