f70f6566aaaa9332f2af757d01e4b02330b3b515
Justin Jiang <justin@Justins-MacBook-Pro-2.local>
2022-10-02 18:30:08 -0400

    Fix the calendar

--- app/src/main/java/com/cornellappdev/scoop/ui/MainActivity.kt
+++ app/src/main/java/com/cornellappdev/scoop/ui/MainActivity.kt
@@ -1,35 +1,22 @@
 package com.cornellappdev.scoop.ui
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.cornellappdev.scoop.ui.components.calendar.*
-import com.cornellappdev.scoop.ui.components.general.Calendar
+import androidx.annotation.RequiresApi
 import com.cornellappdev.scoop.ui.components.general.CalendarScreen
-import com.cornellappdev.scoop.ui.navigation.MainScreen
 import com.cornellappdev.scoop.ui.theme.ScoopTheme
 import com.google.accompanist.insets.ProvideWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.datetime.Month
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContent {
