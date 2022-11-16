3d787fce1c06133bf61e63ee9c9bd5c5b758ce61
Maxwell Pang <mp774@cornell.edu>
2022-11-16 11:07:17 -0500

    Change MainActivity back to running MainScreen

--- app/src/main/java/com/cornellappdev/scoop/ui/MainActivity.kt
+++ app/src/main/java/com/cornellappdev/scoop/ui/MainActivity.kt
@@ -3,11 +3,7 @@ package com.cornellappdev.scoop.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import com.cornellappdev.scoop.data.models.Ride
-import com.cornellappdev.scoop.data.models.User
-import com.cornellappdev.scoop.ui.components.post.MapRideCard
+import com.cornellappdev.scoop.ui.navigation.MainScreen
 import com.cornellappdev.scoop.ui.theme.ScoopTheme
 import com.google.accompanist.insets.ProvideWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,38 +17,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProvideWindowInsets {
                 ScoopTheme {
-                    val placeholderUser = User(
-                        id = 0,
-                        netid = "mp774",
-                        firstName = "Maxwell",
-                        lastName = "Pang",
-                        phoneNumber = "6464311636",
-                        email = "mp774@cornell.edu",
-                        grade = "sophomore",
-                        profilePicUrl = null,
-                        pronouns = "he/him",
-                        hometown = "New York, NY",
-                        song = "drunk by keshi",
-                        snack = "kit kat",
-                        stop = "Shake Shack"
-                    )
-                    val placeholderRide = Ride(
-                        id = 0,
-                        creator = placeholderUser,
-                        maxTravelers = 10,
-                        minTravelers = 1,
-                        departureDatetime = "12:00:00 11/11/2022",
-                        description = "help me pls",
-                        isFlexible = true,
-                        type = "Rideshare",
-                        departureLocation = "Ithaca, NY",
-                        arrivalLocation = "New York, NY",
-                        dateOfTrip = "11/11/2022",
-                        timeOfTrip = "12:00:00",
-                        path = null
-                    )
-
-                    MapRideCard(rideState = remember { mutableStateOf(placeholderRide) })
+                    MainScreen()
                 }
             }
         }
