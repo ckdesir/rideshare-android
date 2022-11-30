dc3e1f00ea8e60a333cf9de237afb4cf3b6d33bb
jiangoj <jiangj2620@gmail.com>
2022-11-30 17:10:57 -0500

    Add LOCATION image

--- app/src/main/java/com/cornellappdev/scoop/ui/screens/RequestScreen.kt
+++ app/src/main/java/com/cornellappdev/scoop/ui/screens/RequestScreen.kt
@@ -1,26 +1,35 @@
 package com.cornellappdev.scoop.ui.screens
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.NearMe
+import androidx.compose.material.icons.outlined.Place
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.cornellappdev.scoop.R
+import com.cornellappdev.scoop.data.models.Ride
 import com.cornellappdev.scoop.ui.theme.DarkGreen
 
 @Composable
 fun RequestScreen(
+    rideState: MutableState<Ride>
 ) {
    // TODO: No actual values just yet. Everything is just filler information.
 
@@ -80,6 +89,56 @@ fun RequestScreen(
         Column() {
 
             Text(fontWeight = FontWeight.Bold, text = "LOCATIONS")
+            Row {
+                Icon(
+                    Icons.Outlined.NearMe,
+                    modifier = Modifier
+                        .padding(end = 15.dp)
+                        .size(24.dp)
+                        .align(CenterVertically),
+                    contentDescription = stringResource(R.string.details_icon_description)
+                )
+                rideState.value.departureLocation?.let { departureLocation ->
+                    Text(
+                        departureLocation,
+                        modifier = Modifier.align(CenterVertically),
+                        style = MaterialTheme.typography.body1,
+                    )
+                }
+            }
+
+            Canvas(
+                Modifier
+                    .height(10.dp)
+                    .wrapContentWidth()
+                    .padding(start = 12.dp)
+            ) {
+                drawLine(
+                    color = Color.Black,
+                    start = Offset(0f, 0f),
+                    end = Offset(0f, size.height),
+                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(8f, 8f), 0f),
+                    strokeWidth = 3f
+                )
+            }
+
+            Row {
+                Icon(
+                    Icons.Outlined.Place,
+                    modifier = Modifier
+                        .padding(end = 15.dp)
+                        .size(24.dp)
+                        .align(CenterVertically),
+                    contentDescription = stringResource(R.string.details_icon_description)
+                )
+                rideState.value.arrivalLocation?.let { arrivalLocation ->
+                    Text(
+                        arrivalLocation,
+                        modifier = Modifier.align(CenterVertically),
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
         }
 
         Column() {
